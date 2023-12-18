cf88c6d12e6106a1bac16618160651d42be0e9d4
HANYUJIN <mooo7570@gmail.com>
2023-12-18 13:10:24 +0900

    Feat: Admin Navbar 구현

---
+++ src/components/AdminNavbar.js
@@ -0,0 +1,44 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import AdminNavItem from './AdminNavItem';
+
+const AdminNavbar = () => {
+  return (
+    <nav css={navbar}>
+      <ul>
+        <AdminNavItem page={'/admin'} title={'대시 보드'} />
+        <AdminNavItem page={'/admin/posts'} title={'게시물 관리'} />
+        <AdminNavItem page={'/admin/application'} title={'등록 신청 내역'} />
+      </ul>
+    </nav>
+  );
+};
+
+export default AdminNavbar;
+
+const navbar = css`
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+  gap: 2rem;
+  box-sizing: border-box;
+  border-right: 1px solid $color-gray;
+  background-color: $color-white;
+  z-index: 200;
+
+  li {
+    font-size: 16px;
+    border-bottom: 1px solid gray;
+  }
+
+  a {
+    padding: 1.2rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: inherit;
+    cursor: pointer;
+    text-decoration: none;
+  }
+`;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import AdminHeader from './AdminHeader';
+import AdminNavbar from './AdminNavbar';
 
 const App = () => {
   const isAdmin = true;
@@ -13,7 +14,14 @@ const App = () => {
       {isAdmin ? (
         <>
           <AdminHeader />
-          <Outlet />
+          <div css={admin_wrap}>
+            <AdminNavbar />
+            <div css={admin_outlet_wrap}>
+              <div css={admin_outlet}>
+                <Outlet />
+              </div>
+            </div>
+          </div>
         </>
       ) : (
         <>
@@ -29,6 +37,30 @@ const App = () => {
 };
 
 export default App;
+
+const admin_wrap = css`
+  display: flex;
+  width: 100vw;
+  height: 90vh;
+`;
+
+const admin_outlet_wrap = css`
+  // border: 1px solid red;
+  width: 100%;
+  background-color: #f2f5e8;
+  padding: 2rem;
+`;
+
+const admin_outlet = css`
+  border: 1px solid black;
+  width: 100%;
+  min-height: 100%;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: $box-shadow-deep;
+  padding: 2rem;
+  box-sizing: border-box;
+`;
 
 const wrapper = css`
   height: auto;
