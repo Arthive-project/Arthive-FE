1094c27bb37677fe8bda2003975ddf008ce31e7e
HANYUJIN <mooo7570@gmail.com>
2024-01-02 13:05:22 +0900

    Feat: 대분류 카테고리 불러오기

--- src/pages/Boards/FestivalAll.js
+++ src/pages/Boards/FestivalAll.js
@@ -1,16 +1,40 @@
-import { getExhibitions } from '../../api';
+import { getPostByCategory } from '../../api/requestAPI';
+import { useState, useEffect } from 'react';
 import BoardHeader from '../../components/BoardHeader';
 import BoardList from '../../components/BoardList';
 import BoardItem from '../../components/BoardItem';
 
 const FestivalAll = () => {
-  const festivalAll = getExhibitions();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const categories = [
+          '축제-전통/역사',
+          '축제-자연/경관',
+          '축제-시민/화합',
+          '축제-문화/예술',
+          '축제-기타',
+        ];
+        const categoryPromises = categories.map((category) =>
+          getPostByCategory(category)
+        );
+        const responses = await Promise.all(categoryPromises);
+        const allPosts = responses.flat();
+        setPosts(allPosts);
+      } catch (error) {
+        console.error('Error', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
       <BoardHeader text='축제' showText='false' subText='전체' />
       <BoardList>
-        {festivalAll.map((festival) => (
+        {posts.map((festival) => (
           <BoardItem
             key={festival.id}
             data={festival}
