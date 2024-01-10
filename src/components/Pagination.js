725b92c684ef5b8846130becffe647356ca6961e
HANYUJIN <mooo7570@gmail.com>
2024-01-10 18:38:40 +0900

    Feat: 페이지네이션 범위 수정

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,23 +1,52 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination = ({ postsPerPage, totalPosts, currentPage, paginate }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
+  }
+
+  const maxPageNumbers = 10;
+  let startPage = 1;
+  let endPage = Math.min(startPage + maxPageNumbers - 1, pageNumbers.length);
+
+  if (currentPage > Math.floor(maxPageNumbers / 2)) {
+    startPage = currentPage - Math.floor(maxPageNumbers / 2);
+    endPage = startPage + maxPageNumbers - 1;
   }
 
   return (
     <div>
       <nav>
         <ul css={page_ul}>
-          {pageNumbers.map((number) => (
+          {currentPage > 1 && (
+            <li css={page_li}>
+              <button onClick={() => paginate(currentPage - 1)} css={page_btn}>
+                {'<'}
+              </button>
+            </li>
+          )}
+          {pageNumbers.slice(startPage - 1, endPage).map((number) => (
             <li key={number} css={page_li}>
-              <button onClick={() => paginate(number)} css={page_btn}>
+              <button
+                onClick={() => paginate(number)}
+                css={page_btn}
+                style={{
+                  fontWeight: currentPage === number ? 'bold' : 'normal',
+                }}
+              >
                 {number}
               </button>
             </li>
           ))}
+          {currentPage < pageNumbers.length && (
+            <li css={page_li}>
+              <button onClick={() => paginate(currentPage + 1)} css={page_btn}>
+                {'>'}
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
